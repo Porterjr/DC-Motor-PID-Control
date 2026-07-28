8ffd91c0c770a662510627af46e4c89fc50799fa
王楷添 <954137731@qq.com>
2026-07-28 22:22:04 +0800

    Day1: 开环PWM占空比测试完成
    - 20/50/80/100四档转速正常
    - 20%占空比可稳定启动
    - 接线图归档

--- User/main.c
+++ User/main.c
@@ -10,17 +10,32 @@ int main(void)
 	
 	while (1)
 	{
-		Motor_SetSpeed(50);
+//		Motor_SetSpeed(50);
+//        Delay_ms(3000);
+//        
+//        Motor_SetSpeed(0);
+//        Delay_ms(1000);
+//        
+//        Motor_SetSpeed(-50);
+//        Delay_ms(3000);
+//        
+//        Motor_SetSpeed(0);
+//        Delay_ms(1000);
+		
+		Motor_SetSpeed(20);    // 20% 占空比
         Delay_ms(3000);
         
-        Motor_SetSpeed(0);
-        Delay_ms(1000);
-        
-        Motor_SetSpeed(-50);
+        Motor_SetSpeed(50);    // 50% 占空比
         Delay_ms(3000);
         
-        Motor_SetSpeed(0);
-        Delay_ms(1000);
+        Motor_SetSpeed(80);    // 80% 占空比
+        Delay_ms(3000);
+        
+        Motor_SetSpeed(100);   // 100% 占空比
+        Delay_ms(3000);
+        
+        Motor_SetSpeed(0);     // 停止
+        Delay_ms(2000);
 	}
 	
 }
