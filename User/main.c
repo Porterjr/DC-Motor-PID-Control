2177744b532bef665f1bbc69e70d95672a3f0b90
王楷添 <954137731@qq.com>
2026-07-30 13:13:07 +0800

    Day3: OLED显示+按键调速+转速计算完成
    - 4行OLED实时显示：Target/Actual/PWM/Delta
    - KEY1/KEY2调速，KEY3停止
    - 编码器计算RPM公式验证通过

--- User/main.c
+++ User/main.c
@@ -4,6 +4,7 @@
 #include "Motor.h"
 #include "Encoder.h"
 #include "Serial.h"
+#include "Key.h"
 
 int main(void)
 {
@@ -11,30 +12,57 @@ int main(void)
 	Motor_Init();
 	Encoder_Init();
     Serial_Init();
+	Key_Init();
 	
-	Serial_Printf("Motor+Encoder Test\r\n");
+	OLED_ShowString(1, 1, "Target:");
+    OLED_ShowString(2, 1, "Actual:");
+    OLED_ShowString(3, 1, "PWM:");
+    OLED_ShowString(4, 1, "Delta:");
 	
+	int16_t TargetSpeed = 0;
 	int16_t lastCount = 0;
 	
 	while (1)
 	{
-		Motor_SetSpeed(30);        // 低速正转，30%占空比
-        Delay_ms(1000);
+		 uint8_t key = Key_GetNum();
+        
+        if (key == 1)
+        {
+            TargetSpeed += 20;
+            if (TargetSpeed > 100) TargetSpeed = 100;
+        }
+        else if (key == 2)
+        {
+            TargetSpeed -= 20;
+            if (TargetSpeed < -100) TargetSpeed = -100;
+        }
+        else if (key == 3)
+        {
+            TargetSpeed = 0;
+        }
+        
+        Motor_SetSpeed(TargetSpeed);
+        Delay_ms(100);
         
         int16_t currentCount = Encoder_GetCount();
         int16_t delta = currentCount - lastCount;
-        
-        Serial_Printf("Delta: %d\r\n", delta);
-        
         lastCount = currentCount;
         
-        if(currentCount > 60000 || currentCount < -60000)
+        int16_t ActualRPM = delta * 600 / 19240;
+        
+        OLED_ShowSignedNum(1, 9, TargetSpeed, 3);
+        OLED_ShowSignedNum(2, 9, ActualRPM, 3);
+        OLED_ShowSignedNum(3, 9, TargetSpeed, 3);
+        OLED_ShowSignedNum(4, 9, delta, 4);
+        
+        Serial_Printf("Tar:%d Act:%d D:%d\r\n", TargetSpeed, ActualRPM, delta);
+        
+        if(currentCount > 30000 || currentCount < -30000)
         {
             TIM_SetCounter(TIM3, 0);
             lastCount = 0;
-		}
+        }
 	}
-	
 }
 
 //		Motor_SetSpeed(50);
@@ -63,3 +91,19 @@ int main(void)
 //        
 //        Motor_SetSpeed(0);     // 停止
 //        Delay_ms(2000);
+
+//		Motor_SetSpeed(30);        // 低速正转，30%占空比
+//        Delay_ms(1000);
+//        
+//        int16_t currentCount = Encoder_GetCount();
+//        int16_t delta = currentCount - lastCount;
+//        
+//        Serial_Printf("Delta: %d\r\n", delta);
+//        
+//        lastCount = currentCount;
+//        
+//        if(currentCount > 60000 || currentCount < -60000)
+//        {
+//            TIM_SetCounter(TIM3, 0);
+//            lastCount = 0;
+//		}
